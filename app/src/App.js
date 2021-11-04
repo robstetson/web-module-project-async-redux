3ca7d00ac304c21e39c8c28db836a2d8b93d7c60
robstetson <rstetsonx@gmail.com>
2021-11-03 20:01:03 -0400

    Api is sending data correctly, made a cool dog app. Did some basic styling, I did follow the guided to a T but keeping it simple led to little to no bugs

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Doggo from './components/dog';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Rob's Random Dog Generator</h1>
+    <Doggo />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { reducer } from './reducers'
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  // <React.StrictMode>
+<Provider store ={store}> 
+    <App />
+    </Provider>,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
